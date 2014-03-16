ab4cc097a1deab19f0118a89fec792cd09baaf1a
Pavel Petrov <ipavelpetrov@ya.ru>
2014-03-16 20:01:14 +0300

    Configure Guard and Spork

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -33,7 +33,8 @@ describe "Static Pages" do
   describe "About page" do
   	it "should have the h1 'About Us'" do
   		visit '/static_pages/about'
- 		page.should have_selector('h1', :text => 'About Us')
+ 		   
+      page.should have_selector('h1', :text => 'About Us')
   	end
 
   	it "should have the title 'About Us'" do
